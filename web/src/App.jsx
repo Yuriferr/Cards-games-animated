f26f9a2896342ffede4f4a8dde6f5ba51fa4a855
Yuriferr <yurifernandespreto@gmail.com>
2023-05-08 21:38:01 -0300

    final commit

---
+++ web/src/App.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react'
+import './App.scss'
+
+import axios from 'axios'
+
+import Cards from './components/Cards'
+
+
+export default function App() {
+
+  const [dados, setDados] = useState({})
+
+  useEffect(() => {
+    getDados()
+  }, [])
+
+  async function getDados(){
+    await axios.get(`http://ddragon.leagueoflegends.com/cdn/13.9.1/data/en_US/champion.json`).then(function (response){
+      setDados(response.data.data)
+    })  
+  }
+
+  return (
+    <div className='App'>
+      {Object.keys(dados).map((item, index) => {
+        return(
+          <Cards key={index} champion={item}/>
+        )
+      })}
+    </div>
+  )
+}
+
+
---
+++ web/src/components/Cards/index.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import './style.scss'
+
+import axios from 'axios'
+
+export default function Cards({champion}){
+
+    const [dados, setDados] = useState({
+        name: '',
+        title: '',
+    })
+    const [color, setColor] = useState('')
+
+    useEffect(() => {
+        getDados()
+    }, [])
+
+    async function getDados(){
+        await axios.get(`http://ddragon.leagueoflegends.com/cdn/13.9.1/data/en_US/champion.json`).then(function (response){
+            setDados({
+                name: response.data.data[champion].name,
+                title: response.data.data[champion].title,
+            })
+        })
+
+        setColor(generateColor())
+    }
+
+    function generateColor() {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        
+        for (let i = 0; i < 6; i++) {
+          color += letters[Math.floor(Math.random() * 16)];
+        }
+        
+        return color;
+      }
+
+    return(
+        <div className="Cards">
+            <h2>{dados.name}</h2>
+            <p>{dados.title}</p>
+            <span>Hover here</span>
+            <div className="pic" style={{backgroundImage: `url(http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion}_0.jpg)`}}/>
+            <button style={{backgroundColor: color}}/>
+        </div>
+    )
+}
